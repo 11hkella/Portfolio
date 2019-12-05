f5df62c41cb55f1c4c4f9b890b6bf51bff6d8265
Henry Kellam <henry.a.kellam@gmail.com>
2019-12-05 16:10:30 -0500

    added carousel component

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import ProjectPiece from './ProjectPiece.js'
+import Carousel from './Carousel.js'
 
 export default class Projects extends Component {
   render() {
@@ -8,10 +9,29 @@ export default class Projects extends Component {
       <div className='project-container'>
         <h1>Projects</h1>
 
-        <ProjectPiece
-          source='images/Flowstate.png'
-          title='Flowstate'
-          technologies={['html5', 'css', 'javascript']} />
+        <Carousel>
+
+          <ProjectPiece
+            source='images/Flowstate.png'
+            title='Flowstate'
+            technologies={['html5', 'css', 'javascript']} />
+
+          <ProjectPiece
+            source='images/WheelOfChores.png'
+            title='Wheel of Chores'
+            technologies={['node', 'handelbars', 'mongoDB', 'mongoose']} />
+
+          <ProjectPiece
+            source='images/SideB.png'
+            title='Side B'
+            technologies={['node', 'react', 'mongoDB', 'mongoose']} />
+
+          <ProjectPiece
+            source='images/EmojiCam.png'
+            title='EmojiCam'
+            technologies={['node', 'react', 'PostgreSQL', 'Django']} />
+
+        </Carousel>
       </div>
     )
   }
