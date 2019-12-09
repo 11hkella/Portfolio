c92873909bfa3dc88550852bd263bb4f0befd8ba
Henry Kellam <henry.a.kellam@gmail.com>
2019-12-09 12:35:10 -0500

    animated vertical card flip on projects

--- src/ProjectPiece.js
+++ src/ProjectPiece.js
@@ -13,21 +13,26 @@ export default class ProjectPiece extends Component {
 
         return (
             <div className='project-piece-container' >
+                <div className='flip-card'>
 
-                <img src={this.props.source} alt='project' />
 
-                <div className='info-container'>
+                    <div className='card-front'>
+                        <img src={this.props.source} alt='project' />
+                    </div>
 
-                    < h3 > {this.props.title}</h3>
 
-                    {
-                        this.props.technologies.map((tech, i) => {
+                    <div className='card-back'>
+                        < h3 > {this.props.title}</h3>
+
+                        {this.props.technologies.map((tech, i) => {
                             return <span className='tech-used' key={i}>
                                 . {tech} .</span>
                         })
-                    }
-                </div>
+                        }
+                    </div>
 
+
+                </div>
             </div >
         )
     }
--- src/Projects.js
+++ src/Projects.js
@@ -1,71 +1,41 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
 
 import ProjectPiece from './ProjectPiece.js'
 
 import './Projects.css'
 
-const Card = styled.div`
-    animation: ${(props) => props.changeCards ?
-        'flip-scale-2-hor-top 0.4s linear both forwards'
-        :
-        'flip-scale-2-hor-top 0.4s linear both reverse'};
-    border: 2px solid black;
-    border-radius: 10px;
-    width: 40vw;
-    overflow: hidden;
-
-`
-
 export default class Projects extends Component {
-
-    state = {
-        changeCards: true,
-        flipIn: false,
-
-    }
-
-    componentDidMount() {
-        setInterval(() => {
-            this.flipCard()
-        }, 10000)
-    }
-
-    flipCard = () => {
-        this.setState({ changeCards: !this.state.changeCards, })
-    }
-
 
     render() {
         return (
-            <div className='project-container' >
+            <div>
                 <h1>Projects</h1>
 
+                <div className='project-container' >
 
-                <Card changeCards={this.state.changeCards}>
-                    {this.state.changeCards ?
-                        <ProjectPiece
-                            source='images/Flowstate.png'
-                            title='Flowstate'
-                            technologies={['html5', 'css', 'javascript']} />
-                        :
-                        <ProjectPiece
-                            source='images/WheelOfChores.png'
-                            title='Wheel of Chores'
-                            technologies={['node', 'handelbars', 'mongoDB', 'mongoose']} />
-                    }
-                </Card>
+                    <ProjectPiece
+                        source='images/Flowstate.png'
+                        title='Flowstate'
+                        technologies={['html5', 'css', 'javascript']} />
 
-                <ProjectPiece
-                    source='images/SideB.png'
-                    title='Side B'
-                    technologies={['node', 'react', 'mongoDB', 'mongoose']} />
+                    <ProjectPiece
+                        source='images/WheelOfChores.png'
+                        title='Wheel of Chores'
+                        technologies={['node', 'handelbars', 'mongoDB', 'mongoose']} />
 
-                <ProjectPiece
-                    source='images/EmojiCam.png'
-                    title='EmojiCam'
-                    technologies={['node', 'react', 'PostgreSQL', 'Django']} />
 
+
+                    <ProjectPiece
+                        source='images/SideB.png'
+                        title='Side B'
+                        technologies={['node', 'react', 'mongoDB', 'mongoose']} />
+
+                    <ProjectPiece
+                        source='images/EmojiCam.png'
+                        title='EmojiCam'
+                        technologies={['node', 'react', 'PostgreSQL', 'Django']} />
+
+                </div >
             </div>
         )
     }
