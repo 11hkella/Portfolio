b6792867c5151266769567dcacfa47c888f59944
Henry Kellam <henry.a.kellam@gmail.com>
2020-01-21 16:14:53 -0500

    updated content and style About

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -9,23 +9,28 @@ export default class AboutMe extends Component {
     render() {
         const delay = this.props.multiplier * 100
         return (
-            <ScrollAnimation
-                animateIn="fadeIn"
-                delay={delay}>
+            <div>
                 <h1 className="about-title">About Me</h1>
                 <div className='about-me-container'>
                     <img className='profile-pic' src='./images/profile1-copy.jpg' alt='profile' />
-                    <div className="wrapper">
-                        <p className='about'>
-                            Experience in full-stack developing with a focus on object-oriented
-        programming, modern frameworks, and time efficiency. Ability to learn
-        quickly with a diverse background in music, lighting design, and
-        mechanical engineering. Inspired when utilizing technical skills to take
-        ambitious projects from concept to creation.
-                </p>
-                    </div>
+
+                    <ScrollAnimation
+                        className='animate-wrapper'
+                        animateIn="fadeIn"
+                        delay={delay}>
+                        <div className="wrapper">
+                            <p className='about'>
+                                <span>Hi there!</span><br />
+                                I'm a web developer with 1 year of experience building full-stack applicaitons using Javascript,
+                                NoSQL/SQL databases, and REST APIs. I think the best part of development is experiencing those
+                                 "Ah-ha" moments that come after working through difficult logic or when finding that obscure solution deep within the docs.
+                                When I'm not coding, I enjoy performing and recording music, going on hikes with my girlfriend, and playing
+                                card games with friends.
+                            </p>
+                        </div>
+                    </ScrollAnimation>
                 </div >
-            </ScrollAnimation>
+            </div>
         )
     }
 }
