1c09c8c96636d59aa9a90d5df1f9abc711e75c30
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-05 20:14:34 -0500

    removed scroll animation on about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -14,21 +14,16 @@ export default class AboutMe extends Component {
                 <div className='about-me-container'>
                     <img className='profile-pic' src={profilePicture} />
 
-                    <ScrollAnimation
-                        className='animate-wrapper'
-                        animateIn="fadeIn"
-                        animateOnce={true}>
-                        <div className="wrapper">
-                            <p className='about'>
-                                <span>Hi there!</span><br />
-                                I'm a web developer with 1 year of experience building full-stack applicaitons using Javascript,
-                                NoSQL/SQL databases, and REST APIs. I think the best part of development is experiencing those
-                                 "Ah-ha" moments that come after working through difficult logic or when finding that obscure solution deep within the docs.
-                                When I'm not coding, I enjoy performing and recording music, going on hikes with my girlfriend, and playing
-                                card games with friends.
+                    <div className="wrapper">
+                        <p className='about'>
+                            <span>Hi there!</span><br />
+                            I'm a web developer with 1 year of experience building full-stack applicaitons using Javascript,
+                            NoSQL/SQL databases, and REST APIs. I think the best part of development is experiencing those
+                             "Ah-ha" moments that come after working through difficult logic or when finding that obscure solution deep within the docs.
+                            When I'm not coding, I enjoy performing and recording music, going on hikes with my girlfriend, and playing
+                            card games with friends.
                             </p>
-                        </div>
-                    </ScrollAnimation>
+                    </div>
                 </div >
             </div>
         )
