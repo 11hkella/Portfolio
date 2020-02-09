863891920915a108bc12c33e87c97095f63ad2cf
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-08 23:48:10 -0500

    implimented footer version 1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Projects from './components/Projects.js'
 import Navbar from './components/Navbar.js'
 import Hero from './components/Hero.js'
 import AboutMe from './components/AboutMe.js'
+import Footer from './components/Footer.js'
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <AboutMe />
       <SkillWheel />
       <Projects />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>henry.a.kellam@gmail.com</p>
+                <p>(630)310-0049</p>
+                <p>Atlanta, Ga</p>
+            </footer>
+        )
+    }
+}
