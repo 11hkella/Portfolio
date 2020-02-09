188510482b9e94572b1e177bfbdcad429268af14
Henry Kellam <henry.a.kellam@gmail.com>
2020-02-09 00:22:29 -0500

    impliment footer version 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,15 @@ export default class Footer extends Component {
     render() {
         return (
             <footer>
-                <p>henry.a.kellam@gmail.com</p>
-                <p>(630)310-0049</p>
-                <p>Atlanta, Ga</p>
+                <ul>
+                    <li><strong>CONTACT</strong></li>
+                    <li>henry.a.kellam@gmail.com</li>
+                    <li>Phone: (630)310-0049</li>
+                </ul>
+
+                <ul>
+                    <li>Henry Kellam &copy; 2019</li>
+                </ul>
             </footer>
         )
     }
