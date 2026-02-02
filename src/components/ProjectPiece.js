533b97cba0929f0b244a57191eb11d75901791a0
Henry Kellam <henry.a.kellam@gmail.com>
2026-02-02 11:43:14 -0500

    remove dead links on projects

--- src/components/ProjectPiece.js
+++ src/components/ProjectPiece.js
@@ -1,57 +1,46 @@
-import React, { Component } from 'react'
-import gitHubIcon from '../icons/github.png'
-import webLinkIcon from '../icons/link-24px.svg'
-import './ProjectPiece.css'
-
+import React, { Component } from "react";
+import gitHubIcon from "../icons/github.png";
+import webLinkIcon from "../icons/link-24px.svg";
+import "./ProjectPiece.css";
 
 export default class ProjectPiece extends Component {
+  render() {
+    return (
+      <div className="project-piece-container">
+        <div className="flip-card">
+          <div className="card-front">
+            <img src={this.props.source} alt="project" />
+          </div>
 
-    render() {
-        return (
-            <div className='project-piece-container' >
-                <div className='flip-card'>
+          <div className="card-back">
+            <p className="description">
+              <span>{this.props.title}</span>
+              {this.props.description}
+            </p>
 
+            <div className="tech-stack">
+              {this.props.technologies.map((tech, i) => {
+                return (
+                  <span className="tech-used" key={i}>
+                    {tech}
+                  </span>
+                );
+              })}
+            </div>
 
-                    <div className='card-front'>
-                        <img src={this.props.source} alt='project' />
-                    </div>
-
-
-                    <div className='card-back'>
-                        < p className='description'>
-                            <span>{this.props.title}</span>{this.props.description}
-                        </p>
-
-                        <div className='tech-stack'>
-                            {this.props.technologies.map((tech, i) => {
-                                return <span
-                                    className='tech-used'
-                                    key={i}>
-                                    {tech}
-                                </span>
-                            })
-                            }
-                        </div>
-
-                        <div className='link-container'>
-                            <a href={this.props.ghLink}
-                                target='blank'>
-                                <img
-                                    src={gitHubIcon}
-                                    alt='github' />
-                            </a>
-                            <a href={this.props.webLink}
-                                target='blank'>
-                                <img
-                                    src={webLinkIcon}
-                                    alt='website' />
-                            </a>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div >
-        )
-    }
+            <div className="link-container">
+              <a href={this.props.ghLink} target="blank">
+                <img src={gitHubIcon} alt="github" />
+              </a>
+              {this.props.webLink ? (
+                <a href={this.props.webLink} target="blank">
+                  <img src={webLinkIcon} alt="website" />
+                </a>
+              ) : null}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,57 +1,52 @@
-import React, { Component } from 'react'
-import ProjectPiece from './ProjectPiece.js'
-import './Projects.css'
-import flowstateImg from '../images/Flowstate.png'
-import wheelOfChoresImg from '../images/WheelOfChores.png'
-import sideBImg from '../images/SideB.png'
-import emojiCamImg from '../images/EmojiCam.png'
-
+import React, { Component } from "react";
+import ProjectPiece from "./ProjectPiece.js";
+import "./Projects.css";
+import flowstateImg from "../images/Flowstate.png";
+import wheelOfChoresImg from "../images/WheelOfChores.png";
+import sideBImg from "../images/SideB.png";
+import emojiCamImg from "../images/EmojiCam.png";
 
 export default class Projects extends Component {
+  render() {
+    return (
+      <div className="project-wrapper">
+        <h1>Projects</h1>
 
-    render() {
-        return (
-            <div className='project-wrapper'>
-                <h1>Projects</h1>
+        <div className="project-container">
+          <ProjectPiece
+            source={flowstateImg}
+            title="Flowstate"
+            description=' is a clone of the popular memory game "Simon", which tests the player&apos;s ability to remember increasingly long sequences of inputs.'
+            technologies={["HTML5", "CSS", "Javascript"]}
+            ghLink="https://github.com/11hkella/Flowstate"
+          />
 
-                <div className='project-container' >
+          <ProjectPiece
+            source={wheelOfChoresImg}
+            title="Wheel of Chores"
+            description=" is a scheduling app that helps groups of users assign and accomplish tasks."
+            technologies={["MongoDB", "Express", "Handelbars", "Node"]}
+            ghLink="https://github.com/11hkella/Wheel-of-Chores"
+          />
 
-                    <ProjectPiece
-                        source={flowstateImg}
-                        title='Flowstate'
-                        description=' is a clone of the popular memory game "Simon", which tests the player&apos;s ability to remember increasingly long sequences of inputs.'
-                        technologies={['HTML5', 'CSS', 'Javascript']}
-                        ghLink='https://github.com/11hkella/Flowstate'
-                        webLink='https://flowstate.netlify.com/' />
+          <ProjectPiece
+            source={sideBImg}
+            title="B-Side"
+            description=" is music reviewing app that allows users to post opinion pieces about their favorite albums, songs, or artists. Write a review yourself, or engauge in a dialogue over the music you are passionate about."
+            technologies={["MongoDB", "Express", "React", "Node"]}
+            ghLink="https://github.com/11hkella/B-side"
+          />
 
-                    <ProjectPiece
-                        source={wheelOfChoresImg}
-                        title='Wheel of Chores'
-                        description=' is a scheduling app that helps groups of users assign and accomplish tasks.'
-                        technologies={['MongoDB', 'Express', 'Handelbars', 'Node']}
-                        ghLink='https://github.com/11hkella/Wheel-of-Chores'
-                        webLink='https://wheel-of-chores.herokuapp.com/wheel' />
-
-
-
-                    <ProjectPiece
-                        source={sideBImg}
-                        title='Side B'
-                        description=' is music reviewing app that allows users to post opinion pieces about their favorite albums, songs, or artists. Write a review yourself, or engauge in a dialogue over the music you are passionate about.'
-                        technologies={['MongoDB', 'Express', 'React', 'Node']}
-                        ghLink='https://github.com/11hkella/B-side'
-                        webLink='https://b-side-review.herokuapp.com/' />
-
-                    <ProjectPiece
-                        source={emojiCamImg}
-                        title='EmojiCam'
-                        description=' is a video app that uses facial recognition and expression detection to automatically map various emojis to user&apos;s faces.'
-                        technologies={['PostgreSQL', 'Django', 'React', 'Node']}
-                        ghLink='https://github.com/11hkella/EmojiCam'
-                        webLink='https://master.dewt15ge7jmum.amplifyapp.com/' />
-
-                </div >
-            </div>
-        )
-    }
+          <ProjectPiece
+            source={emojiCamImg}
+            title="EmojiCam"
+            description=" is a video app that uses facial recognition and expression detection to automatically map various emojis to user's faces."
+            technologies={["PostgreSQL", "Django", "React", "Node"]}
+            ghLink="https://github.com/11hkella/EmojiCam"
+            webLink="https://master.dewt15ge7jmum.amplifyapp.com/"
+          />
+        </div>
+      </div>
+    );
+  }
 }
